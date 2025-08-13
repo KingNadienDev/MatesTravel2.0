459aa161c1b142e9ef31520fe13c91fc0200ae82
SrNadien 🔥 <76491773+SrNadien@users.noreply.github.com>
2025-08-13 14:45:57 -0300

    recoded index

--- javascript/navbar-utilities.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', () => {
-  const nav = document.querySelector('.navbar');
-  if (window.pageYOffset > 0) nav.classList.add('scrolled');
-  else nav.classList.remove('scrolled');
-});
